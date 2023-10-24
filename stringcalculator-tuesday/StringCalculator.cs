a9204057ede52e69662c1c31f06c6178e0e8fea0
Jeffry Gonzalez <jeff@hypertheory.com>
2023-10-24 14:17:39 -0400

    Doing String Calculator

---
+++ stringcalculator-tuesday/StringCalculator.cs
@@ -0,0 +1,22 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+	public int Add(string numbers)
+	{
+		if (numbers.Contains(','))
+		{
+			return 2;
+		}
+		if (numbers == "")
+		{
+			return 0;
+		}
+		else
+		{
+			return int.Parse(numbers);
+		}
+	}
+}
---
+++ stringcalculator-tuesday/StringCalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+	[Fact]
+	public void EmptyStringReturnsZero()
+	{
+		var calculator = new StringCalculator();
+
+		var result = calculator.Add("");
+
+		Assert.Equal(0, result);
+	}
+
+	[Theory]
+	[InlineData("1", 1)]
+	[InlineData("2", 2)]
+	[InlineData("118", 118)]
+	public void SingleDigit(string input, int expected)
+	{
+		var calculator = new StringCalculator();
+		var result = calculator.Add(input);
+		Assert.Equal(expected, result);
+	}
+
+	[Theory]
+	[InlineData("1,1", 2)]
+	[InlineData("10,2", 12)]
+	public void TwoDigits(string input, int expected)
+	{
+		var calculator = new StringCalculator();
+		var result = calculator.Add(input);
+		Assert.Equal(expected, result);
+	}
+}
